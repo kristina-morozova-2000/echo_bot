5010a0ca1959a9d5f36db10d796d2ae3b7ecc0b8
kristina-morozova-2000 <krismorozovamei1@gmail.com>
2022-03-29 15:56:15 +0300

    Изменение main.py
    Добавление хэндлера: функция, выводящая на экран приветственное сообщение

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ from dotenv import load_dotenv
 #Экземпляр бота
 load_dotenv()
 bot = telebot.TeleBot(os.getenv('TOKEN_BOT'))
+
+#Сообщение-приветствие
+@bot.message_handler(commands=['start', 'help'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет! Я - эхо-бот. Напиши что-нибудь, и я это повторю.')
